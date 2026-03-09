fae6346e8f581a89f6fce6d80f1bebc8b4e6ff52
RIMJHIM22 <arimjhim22@gmail.com>
2026-03-09 16:22:08 +0530

    day14_task1

---
+++ tests/Day14_task1.spec.ts
@@ -0,0 +1,11 @@
+import { test, expect } from '@playwright/test';
+test('Login validation test', async ({ page }) => {
+  await page.setDefaultTimeout(20000)
+  await page.goto('https://demoapps.qspiders.com/ui/login');
+  await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
+  await page.getByPlaceholder('Enter your email').fill('test@gmail.com');
+  await page.getByPlaceholder('Enter your password').fill('test@123');
+  await expect(page.getByRole('button', { name: 'Login' })).toBeEnabled();
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page).toHaveURL(/dashboard|login/);
+});
